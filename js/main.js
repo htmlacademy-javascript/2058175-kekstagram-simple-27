ca0f55c0d25848708704a2f30793f1d2306a3929
Liza Nemzer <zernemzer@gmail.com>
2022-09-29 23:53:17 +0300

    добавляет функцию проверки длины строки

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-// sorce: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-/* eslint-disable no-console */
-function getRandomArbitrary(firstNumber, secondNumber) {
-  if(firstNumber < 0 || secondNumber < 0) {return NaN;}
-  else if(secondNumber < firstNumber) {return Math.round(Math.random() * (firstNumber - secondNumber) + secondNumber);}
+// source: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function getRandomNumber(firstNumber, secondNumber) {
+  if (firstNumber < 0 || secondNumber < 0) {
+    return NaN;
+  } else if (secondNumber < firstNumber) {
+    return Math.round(
+      Math.random() * (firstNumber - secondNumber) + secondNumber
+    );
+  }
   return Math.round(Math.random() * (secondNumber - firstNumber) + firstNumber);
 }
 
-getRandomArbitrary(4, 7);
+getRandomNumber();
+
+function checkStringLength(string, maxLength) {
+  if (string.length <= maxLength) {
+    return true;
+  }
+  return false;
+}
+
+checkStringLength();
