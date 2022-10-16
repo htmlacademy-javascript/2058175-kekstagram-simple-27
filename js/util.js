201b6e082a16fbd3ec509227b8cc80f16c567a86
Liza Nemzer <zernemzer@gmail.com>
2022-10-16 17:00:38 +0300

    разделяет js код на модули

---
+++ js/util.js
@@ -0,0 +1,25 @@
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+
+function getRandomPositiveInteger(a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+// function checkStringLength(string, length) {
+//   return string.length <= length;
+// }
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+// checkStringLength();
+// getRandomArrayElement();
+
+export {getRandomPositiveInteger};
+export {getRandomArrayElement};
